fb970ad0de20cbe81e13d8160310693ef63d2cf8
dailcoyote <yergaliyev.production@gmail.com>
2024-06-05 21:48:33 +0500

    @lib:photofilters functions

---
+++ src/lib.rs
@@ -0,0 +1,181 @@
+use image::DynamicImage;
+use rand::Rng;
+
+pub struct CropPayload {
+    pub x: u32,
+    pub y: u32,
+    pub width: u32,
+    pub height: u32,
+}
+
+pub struct RGBColor {
+    pub red: u8,
+    pub blue: u8,
+    pub green: u8,
+}
+
+pub enum RotateDegrees {
+    OneFouth = 90,
+    Half = 180,
+    ThreeQuarters = 270,
+}
+
+#[derive(PartialEq)]
+enum GreenIntensity {
+    Up = 1,
+    Down = 0,
+}
+
+/*      F I L T E R S    */
+
+pub fn blur(source: &DynamicImage, blur_amount: f32) -> DynamicImage {
+    // Parse the blur amount (an f32) from the command-line and pass it through
+    // to this function, instead of hard-coding it to 2.0.
+    return source.blur(blur_amount);
+}
+
+pub fn brighten(source: &DynamicImage, brightness_amount: i32) -> DynamicImage {
+    // .brighten() takes one argument, an i32.  Positive numbers brighten the
+    // image. Negative numbers Downen it.  It returns a new image.
+    return source.brighten(brightness_amount);
+}
+
+pub fn crop(source: &mut DynamicImage, payload: CropPayload) -> DynamicImage {
+    // .crop() takes four arguments: x: u32, y: u32, width: u32, height: u32
+    // You may hard-code them, if you like.  It returns a new image.
+
+    // Challenge: parse the four values from the command-line and pass them
+    // through to this function.
+    return source.crop(payload.x, payload.y, payload.width, payload.height);
+}
+
+pub fn rotate(source: &DynamicImage, degree_option: RotateDegrees) -> DynamicImage {
+    // There are 3 rotate functions to choose from (all clockwise):
+    //   .rotate90()
+    //   .rotate180()
+    //   .rotate270()
+    // All three methods return a new image.  Pick one and use it!
+
+    // Challenge: parse the rotation amount from the command-line, pass it
+    // through to this function to select which method to call.
+    let rotated_img: DynamicImage = match degree_option {
+        RotateDegrees::OneFouth => source.rotate90(),
+        RotateDegrees::Half => source.rotate180(),
+        RotateDegrees::ThreeQuarters => source.rotate270(),
+    };
+    return rotated_img;
+}
+
+pub fn invert(source: &mut DynamicImage) {
+    // .invert() takes no arguments and converts the image in-place, so you
+    // will use the same image to save out to a different file.
+    source.invert();
+}
+
+pub fn grayscale(source: &DynamicImage) -> DynamicImage {
+    // .grayscale() takes no arguments. It returns a new image.
+    return source.grayscale();
+}
+
+pub fn generate(rgb_color: RGBColor, outfile: String) {
+    // Create an ImageBuffer -- see fractal() for an example
+    let width = 1000;
+    let height = 1000;
+
+    let mut imgbuf = image::ImageBuffer::new(width, height);
+
+    // Iterate over the coordinates and pixels of the image -- see fractal() for an example
+    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
+        // Set the image to some solid color. -- see fractal() for an example
+        // Challenge: parse some color data from the command-line, pass it through
+        // to this function to use for the solid color.
+        let red = rgb_color.red;
+        let blue = rgb_color.blue;
+        let green = rgb_color.green;
+
+        *pixel = image::Rgb([red, green, blue]);
+    }
+
+    imgbuf.save(outfile).unwrap();
+}
+
+pub fn gradient(outfile: String) {
+    // Create an ImageBuffer -- see fractal() for an example
+    let width = 1000;
+    let height = 1000;
+
+    const MAX_RGB_INTENSITY: u8 = 255;
+    const MIN_RGB_INTENSITY: u8 = 0;
+
+    let mut imgbuf = image::ImageBuffer::new(width, height);
+    let mut temp_y = 0;
+    let mut green_intensity = GreenIntensity::Up;
+    let mut rng = rand::thread_rng();
+    let red = rng.gen_range(0..255) as u8;
+    let blue = rng.gen_range(0..255) as u8;
+    let mut green = 0;
+
+    // Iterate over the coordinates and pixels of the image -- see fractal() for an example
+    for (_, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        // Challenge 2: Generate something more interesting!
+
+        if temp_y != y {
+            temp_y = y;
+            green = 0;
+            green_intensity = GreenIntensity::Up;
+        }
+
+        if green == MAX_RGB_INTENSITY {
+            green_intensity = GreenIntensity::Down;
+        } 
+        if green == MIN_RGB_INTENSITY {
+            green_intensity = GreenIntensity::Up;
+        }
+        if green_intensity == GreenIntensity::Up && green < MAX_RGB_INTENSITY {
+            green += 1;
+        } 
+        if green_intensity == GreenIntensity::Down && green > MIN_RGB_INTENSITY {
+            green -= 1;
+        }  
+
+        *pixel = image::Rgb([red, green, blue]);
+    }
+
+    imgbuf.save(outfile).unwrap();
+}
+
+// This code was adapted from https://github.com/PistonDevelopers/image
+pub fn fractal(outfile: String) {
+    let width = 800;
+    let height = 800;
+
+    let mut imgbuf = image::ImageBuffer::new(width, height);
+
+    let scale_x = 3.0 / width as f32;
+    let scale_y = 3.0 / height as f32;
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        // Use red and blue to be a pretty gradient background
+        let red = (0.3 * x as f32) as u8;
+        let blue = (0.3 * y as f32) as u8;
+
+        // Use green as the fractal foreground (here is the fractal math part)
+        let cx = y as f32 * scale_x - 1.5;
+        let cy = x as f32 * scale_y - 1.5;
+
+        let c = num_complex::Complex::new(-0.4, 0.6);
+        let mut z = num_complex::Complex::new(cx, cy);
+
+        let mut green = 0;
+        while green < 255 && z.norm() <= 2.0 {
+            z = z * z + c;
+            green += 1;
+        }
+
+        // Actually set the pixel. red, green, and blue are u8 values!
+        *pixel = image::Rgb([red, green, blue]);
+    }
+
+    imgbuf.save(outfile).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,201 @@
+// Create an image processing application.  Exactly what it does and how it does
+// it is up to you, though I've stubbed a good amount of suggestions for you.
+// Look for comments labeled **OPTION** below.
+//
+// Two image files are included in the project root for your convenience: dyson.png and pens.png
+// Feel free to use them or provide (or generate) your own images.
+//
+// Don't forget to have fun and play around with the code!
+//
+// Documentation for the image library is here: https://docs.rs/image/0.21.0/image/
+//
+// Image processing is very CPU-intensive.  Your program will run *noticeably* faster if you
+// run it with the `--release` flag.
+//
+//     cargo run --release [ARG1 [ARG2]]
+//
+// For example:
+//
+//     cargo run --release blur assets/pens.png assets/pens_blurred.png 2.5
+//
+
+use photo_filters::{
+    blur,
+    brighten,
+    crop,
+    rotate,
+    invert,
+    grayscale,
+    generate,
+    gradient,
+    fractal,
+    CropPayload,
+    RGBColor,
+    RotateDegrees
+};
+
 fn main() {
-    println!("Hello, world!");
+    // 1. First, you need to implement some basic command-line argument handling
+    // so you can make your program do different things.  Here's a little bit
+    // to get you started doing manual parsing.
+    //
+    // Challenge: If you're feeling really ambitious, you could delete this code
+    // and use the "clap" library instead: https://docs.rs/clap/2.32.0/clap/
+    let mut args: Vec<String> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        print_usage_and_exit();
+    }
+    let subcommand = args.remove(0);
+    match subcommand.as_str() {
+        // EXAMPLE FOR CONVERSION OPERATIONS
+        "blur" => {
+            if args.len() != 3 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            let blur_amount: f32 = args.remove(0).parse().unwrap();
+            // Here's how you open an existing image file
+            let source_img = image::open(infile).expect("Failed to open INFILE.");
+            blur(&source_img, blur_amount)
+                .save(outfile)
+                .expect("Failed writing OUTFILE.");
+        }
+
+        // **OPTION**
+        // Brighten -- see the brighten() function below
+        "brighten" => {
+            if args.len() != 3 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            let brightness_amount: i32 = args.remove(0).parse().unwrap();
+            // Here's how you open an existing image file
+            let source_img = image::open(infile).expect("Failed to open INFILE.");
+            brighten(&source_img, brightness_amount)
+                .save(outfile)
+                .expect("Failed writing OUTFILE.");
+        }
+
+        // **OPTION**
+        // Crop -- see the crop() function below
+        "crop" => {
+            if args.len() != 6 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            let crop_payload = CropPayload {
+                x: args.remove(0).parse().unwrap(),
+                y: args.remove(0).parse().unwrap(),
+                width: args.remove(0).parse().unwrap(),
+                height: args.remove(0).parse().unwrap(),
+            };
+            let mut source_img = image::open(infile).expect("Failed to open INFILE.");
+            crop(&mut source_img, crop_payload)
+                .save(outfile)
+                .expect("Failed writing OUTFILE.");
+        }
+
+        // **OPTION**
+        // Rotate -- see the rotate() function below
+        "rotate" => {
+            if args.len() != 3 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            let degree_input: i32 = args.remove(0).parse().unwrap();
+            let degree_option = if degree_input == 180 {
+                RotateDegrees::Half
+            } else if degree_input == 270 {
+                RotateDegrees::ThreeQuarters
+            } else {
+                RotateDegrees::OneFouth
+            };
+
+            let source_img = image::open(infile).expect("Failed to open INFILE.");
+            rotate(&source_img, degree_option)
+                .save(outfile)
+                .expect("Failed writing OUTFILE.");
+        }
+
+        // **OPTION**
+        // Invert -- see the invert() function below
+        "invert" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            // Here's how you open an existing image file
+            let mut source_img = image::open(infile).expect("Failed to open INFILE.");
+            invert(&mut source_img);
+            source_img.save(outfile).expect("Failed writing OUTFILE.");
+        }
+
+        // **OPTION**
+        // Grayscale -- see the grayscale() function below
+        "grayscale" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            // Here's how you open an existing image file
+            let source_img = image::open(infile).expect("Failed to open INFILE.");
+            grayscale(&source_img)
+                .save(outfile)
+                .expect("Failed writing OUTFILE.");
+        }
+
+        // A VERY DIFFERENT EXAMPLE...a really fun one. :-)
+        "fractal" => {
+            if args.len() != 1 {
+                print_usage_and_exit();
+            }
+            let outfile = args.remove(0);
+            fractal(outfile);
+        }
+
+        // **OPTION**
+        // Generate -- see the generate() function below -- this should be sort of like "fractal()"!
+        "generate" => {
+            if args.len() != 4 {
+                print_usage_and_exit();
+            }
+            let outfile = args.remove(0);
+            let rgb_color = RGBColor {
+                red: args.remove(0).parse().unwrap(),
+                blue: args.remove(0).parse().unwrap(),
+                green: args.remove(0).parse().unwrap()
+            };
+            generate(rgb_color, outfile);
+        }
+
+        "gradient" => {
+            if args.len() != 1 {
+                print_usage_and_exit();
+            }
+            let outfile = args.remove(0);
+            gradient(outfile);
+        }
+
+        // For everything else...
+        _ => {
+            print_usage_and_exit();
+        }
+    }
+}
+
+fn print_usage_and_exit() {
+    println!("~~~PHOTO FILTERS~~~");
+    println!("USAGE (when in doubt, use a .png extension on your filenames)");
+    println!("blur INFILE OUTFILE");
+    println!("fractal OUTFILE");
+    // **OPTION**
+    // Print useful information about what subcommands and arguments you can use
+    // println!("...");
+    std::process::exit(-1);
 }
